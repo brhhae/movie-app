a6ed9540a138e39a502e1a705324a1286186e3fc
elian <jacobmachado80@protonmail.com>
2022-07-21 17:06:19 +0200

    introducing backend

---
+++ be/src/main/java/com/seyda/movieapp/entities/Movies.java
@@ -0,0 +1,18 @@
+package com.seyda.movieapp.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Movies {
+  @Id
+  private Long id;
+}
